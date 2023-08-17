9cb4c32a80f6e18947ac92130f384409faf17f4a
JOHNSON-GM <gichukijohnson93@gmail.com>
2023-08-18 00:47:15 +0300

    edited

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -9,6 +9,8 @@ for i in range(10000):
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
         datetime.datetime.now(),
+        '/projects/1216',
+        'HTTP/1.1'.
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
